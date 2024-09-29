4cc8e587d2c364aa7fd2c7577ed8951348d75ad8
paksole <tyukunkka@gmail.com>
2024-09-29 12:34:03 +0300

    First test of git

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Добро пожаловать в калькулятор 3.0!");
+        System.out.println("Test1");
+        System.out.println("Test2");
+        System.out.println("Test3");
+        System.out.println("Test4");
+        System.out.println("Test5");
+        System.out.println("Test6");
+        calculate();
+    }
+
+    public static void calculate() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое число:");
+        int operand1 = scanner.nextInt(); //считывает число
+        System.out.println("Введние операцию:");
+        char operation = scanner.next().charAt(0);
+        System.out.println("Введите второе число:");
+        int operand2 = scanner.nextInt();
+
+        System.out.println("Результат:");
+        switch (operation) {
+            case ('+'):
+                System.out.println(operand1 + operand2);
+                break;
+            case ('-'):
+                System.out.println(operand1 - operand2);
+                break;
+            case ('*'):
+                System.out.println(operand1 * operand2);
+                break;
+            case ('/'): {
+                if (operand2 == 0) {
+                    System.out.println("Ошибка: деление на ноль");
+                    break;
+                } else {
+                    System.out.println(operand1 / operand2);
+                    break;
+                }
+            }
+        }
+    }
+}
