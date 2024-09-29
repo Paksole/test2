5061db7f72f48e121b13612ced8f8444b7c63844
paksole <tyukunkka@gmail.com>
2024-09-29 14:38:40 +0300

    var 1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Добро пожаловать в калькулятор 3.0!");
+        System.out.println("Добро пожаловать в калькулятор 1.0!");
 
 
     }
